59a2c94074eabd6781c58a37015f6edf5f73681e
zhangxp93 <937097345@qq.com>
2023-04-15 23:43:38 +0800

    main

---
+++ cpp_source/ch01/ch01.1.3.cpp
@@ -0,0 +1,9 @@
+//练习1.3: 编写程序，在标准输出上打印Hello, World。
+#include<iostream>  
+
+int main()
+{
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+
+}
---
+++ cpp_source/ch01/ch01.1.8.cpp
@@ -0,0 +1,12 @@
+//指出下列哪些输出语句是合法的（如果有的话）：
+#include <iostream>
+int main()
+{
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout <<"the sum of" << v1 <<"and" << v2
+            <<"is"  << v1 + v2 << std::endl;
+
+
+    
+}
